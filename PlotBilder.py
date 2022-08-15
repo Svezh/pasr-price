484394cb0a9a2ef90b3ad8e6e376de9648358dfa
C:/Program Files/Git/Svezh <68671296+Svezh@users.noreply.github.com>
2022-08-15 22:05:32 +0300

    Добавлены исключения при пустом совпадении референсов

--- PlotBilder.py
+++ PlotBilder.py
@@ -13,43 +13,95 @@ pd.options.mode.chained_assignment = None
 
 
 def PlotBilder(ref):
-    if ref == "5E 650i":
-        df_ref = pd.DataFrame(pd.read_csv('refs/5E_650i_ref.csv', sep=','))
-    elif ref == "5E 850i":
-        df_ref = pd.DataFrame(pd.read_csv('refs/5E_850i_ref.csv', sep=','))
-    elif ref == "5E 1000i":
-        df_ref = pd.DataFrame(pd.read_csv('refs/5E_1000i_ref.csv', sep=','))
-    elif ref == "5E 1500i":
-        df_ref = pd.DataFrame(pd.read_csv('refs/5E_1500i_ref.csv', sep=','))
-    elif ref == "5P 650IR":
-        df_ref = pd.DataFrame(pd.read_csv('refs/5P_650IR_ref.csv', sep=','))
-    elif ref == "5P 850IR":
-        df_ref = pd.DataFrame(pd.read_csv('refs/5P_850IR_ref.csv', sep=','))
-    elif ref == "5P 1150I":
-        df_ref = pd.DataFrame(pd.read_csv('refs/5P_1150i_ref.csv', sep=','))
-    elif ref == "5SC 500i":
-        df_ref = pd.DataFrame(pd.read_csv('refs/5SC_500i_ref.csv', sep=','))
-    elif ref == "5SC 750i":
-        df_ref = pd.DataFrame(pd.read_csv('refs/5SC_750i_ref.csv', sep=','))
-    elif ref == "5SC 1000i":
-        df_ref = pd.DataFrame(pd.read_csv('refs/5SC_1000i_ref.csv', sep=','))
-    elif ref == "5E 500i":
-        df_ref = pd.DataFrame(pd.read_csv('refs/5SC_500i_ref.csv', sep=','))
+    def ref_search(ref):
 
-    df_xcm = pd.DataFrame(pd.read_csv('database_xcom_make.csv'))
-    df_stl = pd.DataFrame(pd.read_csv('database_stl_make.csv'))
+        if ref == "5E 650i":
+            df_ref = pd.DataFrame(pd.read_csv('refs/5E_650i_ref.csv', sep=','))  #
+        elif ref == "5E 850i":
+            df_ref = pd.DataFrame(pd.read_csv('refs/5E_850i_ref.csv', sep=','))  #
+        elif ref == "5E 1000i":
+            df_ref = pd.DataFrame(pd.read_csv('refs/5E_1000i_ref.csv', sep=','))  #
+        elif ref == "5E 1500i":
+            df_ref = pd.DataFrame(pd.read_csv('refs/5E_1500i_ref.csv', sep=','))  #
+        elif ref == "5E 2000i":
+            df_ref = pd.DataFrame(pd.read_csv('refs/5E_2000i_ref.csv', sep=','))  #
 
- 
+        elif ref == "5P 650IR":
+            df_ref = pd.DataFrame(pd.read_csv('refs/5P_650IR_ref.csv', sep=','))  #
 
-    def converter(converting_df,df_ref):
+        elif ref == "5P 850IR":
+            df_ref = pd.DataFrame(pd.read_csv('refs/5P_850IR_ref.csv', sep=','))  #
+
+        elif ref == "5P 1550IR":
+            df_ref = pd.DataFrame(pd.read_csv('refs/5P_1550IR_ref.csv', sep=','))  #
+
+        elif ref == "5P 1550IRT":
+            df_ref = pd.DataFrame(pd.read_csv('refs/5P_1550IRT_ref.csv', sep=','))  #
+
+        elif ref == "5SC 500i":
+            df_ref = pd.DataFrame(pd.read_csv('refs/5SC_500i_ref.csv', sep=','))  #
+
+        elif ref == "5SC 750i":
+            df_ref = pd.DataFrame(pd.read_csv('refs/5SC_750i_ref.csv', sep=','))  #
+
+        elif ref == "5SC 1000i":
+            df_ref = pd.DataFrame(pd.read_csv('refs/5SC_1000i_ref.csv', sep=','))  #
+
+        elif ref == "5SC 1500i":
+            df_ref = pd.DataFrame(pd.read_csv('refs/5SC_1500i_ref.csv', sep=','))  #
+
+        elif ref == "5SC 1000IR":
+            df_ref = pd.DataFrame(pd.read_csv('refs/5SC_1500ir_ref.csv', sep=','))  #
+
+        elif ref == "5SC 1500IR":
+            df_ref = pd.DataFrame(pd.read_csv('refs/5SC_1500ir_ref.csv', sep=','))  #
+
+        elif ref == "5SC 2200IRT":
+            df_ref = pd.DataFrame(pd.read_csv('refs/5SC_2200IRT_ref.csv', sep=','))  #
+
+        elif ref == "5SC 3000IRT":
+            df_ref = pd.DataFrame(pd.read_csv('refs/5SC_3000IRT_ref.csv', sep=','))  #
+
+        elif ref == "5E 500i":
+            df_ref = pd.DataFrame(pd.read_csv('refs/5E_500i_ref.csv', sep=','))  #
+
+        elif ref == "3S 450DIN":
+            df_ref = pd.DataFrame(pd.read_csv('refs/3S_450DIN_ref.csv', sep=','))  #
+
+        elif ref == "3S 550DIN":
+            df_ref = pd.DataFrame(pd.read_csv('refs/33S_550DIN_ref.csv', sep=','))  #
+
+        elif ref == "3S 700DIN":
+            df_ref = pd.DataFrame(pd.read_csv('refs/3S_700DIN_ref.csv', sep=','))  #
+
+        elif ref == "3S 850DIN":
+            df_ref = pd.DataFrame(pd.read_csv('refs/3S_850DIN_ref.csv', sep=','))  #
+
+        elif ref == "5PX 1500iRT":
+            df_ref = pd.DataFrame(pd.read_csv('refs/5PX_1500iRT_ref.csv', sep=','))  #
+
+        elif ref == "5PX 2200iRT":
+            df_ref = pd.DataFrame(pd.read_csv('refs/5PX_2200iRT_ref.csv', sep=','))  #
+
+        elif ref == "5PX 3000iRT":
+            df_ref = pd.DataFrame(pd.read_csv('refs/5PX_3000iRT_ref.csv', sep=','))  #
+
+        elif ref == "5PX 3000iRTN":
+            df_ref = pd.DataFrame(pd.read_csv('refs/5PX_3000iRTN_ref.csv', sep=','))  #
+
+
+        return df_ref
+
+    def converter(converting_df, df_ref):
         for i in range(int(len(converting_df['Model']))):
             converting_df['Model'][i] = converting_df['Model'][i].replace('-', '')
             converting_df['Model'][i] = converting_df['Model'][i].replace(' ', '')
             converting_df['Model'][i] = converting_df['Model'][i].upper()
 
         converting_df = converting_df.replace(np.nan, '0', regex=True)
-        converting_df = converting_df.fillna('0')
-        df_comp = converting_df.loc[(converting_df['Model'].isin(df_ref['ART_Model'])) | (converting_df['Model'].isin(df_ref['Name_Model']))]
+        converting_df = converting_df.fillna('1')
+        df_comp = converting_df.loc[
+            (converting_df['Model'].isin(df_ref['ART_Model'])) | (converting_df['Model'].isin(df_ref['Name_Model']))]
 
         mask = df_comp.duplicated(subset=['Model', 'Date', 'Time'])
         df_comp = df_comp.loc[~mask]
@@ -59,21 +111,21 @@ def PlotBilder(ref):
 
 
 
+
     def checker_rule(df_for_check):
 
+        global df_res
         checker_1 = []
         checker_min = []
         checker_max = []
-        date=[]
-        del_date=[]
+        date = []
+        del_date = []
         del_time = []
-        a=[]
+        a = []
 
+        dict_group = df_for_check.groupby(by=['Time', 'Date']).groups
 
-        dict_group=df_for_check.groupby(by=['Time', 'Date']).groups
-
-        date=[*dict_group]
-
+        date = [*dict_group]
 
         for i in range(int(len(date))):
             if date[i][0] == date[i - 1][0]:
@@ -86,49 +138,39 @@ def PlotBilder(ref):
             if checker[i] != max(checker):
                 checker_1.append(checker[i])
 
-
         for i in range(int(len(checker_1))):
             del_time.append(checker_1[i][1])
             del_date.append(checker_1[i][0])
             a.append(dict_group.get(checker[i]))
 
-
         for i in range(len(a)):
-            df_res = df_for_check.drop(index=a[i])
-
-        return df_res
-
-
+            df_for_check = df_for_check.drop(index=a[i])
+        final_df = df_for_check
+        return final_df
 
 
-
-    df_comp=converter(df_xcm,df_ref)
-    df_comp_cit=converter(df_stl,df_ref)
-
-    df_comp = checker_rule(df_comp)
-    df_comp_cit = checker_rule(df_comp_cit)
-    df_comp_cit.reset_index(drop=True, inplace=True)
-    df_comp.reset_index(drop=True, inplace=True)
-
-
-
-
-
-
-
-
-    df_ref_xcm = df_ref.loc[
-        (df_ref['ART_Model'].isin(df_comp['Model'])) | (df_ref['Name_Model'].isin(df_comp['Model']))]
-    df_ref_xcm.reset_index(drop=True, inplace=True)
-    df_ref_cit = df_ref.loc[
-        (df_ref['ART_Model'].isin(df_comp_cit['Model'])) | (df_ref['Name_Model'].isin(df_comp_cit['Model']))]
-    df_ref_cit.reset_index(drop=True, inplace=True)
+    df_xcm = pd.DataFrame(pd.read_csv('database_xcom_make.csv'))
+    df_stl = pd.DataFrame(pd.read_csv('database_stl_make.csv'))
+    df_ref = ref_search(ref)
+    df_comp = converter(df_xcm, df_ref)
+    df_comp_cit = converter(df_stl, df_ref)
+    flag=0
 
     fig, ax = plt.subplots(2, 1)
-    ###Проверка на заполненность датафрема проверки
-    if df_comp_cit.empty:
+
+    if df_comp.empty & df_comp_cit.empty:
+        ax[0].plot(0, 0, label="нет совпадений по референсам")
         ax[1].plot(0, 0, label="нет совпадений по референсам")
-    else:
+    elif    df_comp.empty:
+        ax[0].plot(0, 0, label="нет совпадений по референсам")
+
+
+        df_comp_cit = checker_rule(df_comp_cit)
+        df_comp_cit.reset_index(drop=True, inplace=True)
+        df_ref_cit = df_ref.loc[
+            (df_ref['ART_Model'].isin(df_comp_cit['Model'])) | (
+                df_ref['Name_Model'].isin(df_comp_cit['Model']))]
+        df_ref_cit.reset_index(drop=True, inplace=True)
 
         annotate_mass_x_cit = []
         annotate_mass_y_cit = []
@@ -145,16 +187,17 @@ def PlotBilder(ref):
 
         for j in range(int(len(df_ref_cit['ART_Model']))):
             label = df_comp_cit['Model'][j] + " " + df_comp_cit['Vendor'][j]
-            lebelno = df_comp_cit['Model'][j] + " " + df_comp_cit['Vendor'][j] + "Eaton no"
             data_x = df_comp_cit['Time'].loc[(df_comp_cit['Model'] == df_ref_cit['Name_Model'][j]) | (
-                        df_comp_cit['Model'] == df_ref_cit['ART_Model'][j])]
+                    df_comp_cit['Model'] == df_ref_cit['ART_Model'][j])]
             data_y = df_comp_cit['Current_Price'].loc[(df_comp_cit['Model'] == df_ref_cit['Name_Model'][j]) | (
-                        df_comp_cit['Model'] == df_ref_cit['ART_Model'][j])]
+                    df_comp_cit['Model'] == df_ref_cit['ART_Model'][j])]
             ax[1].plot(data_x, data_y, '.-', label=label, linewidth=2)
 
-            data_label_cit = {'x': annotate_mass_x_cit, 'y': annotate_mass_y_cit}
-            df_label_cit = pd.DataFrame(data_label_cit)
-            df_label_cit.reset_index(drop=True, inplace=True)
+
+        data_label_cit= {'x': annotate_mass_x_cit, 'y': annotate_mass_y_cit}
+        df_label_cit = pd.DataFrame(data_label_cit)
+        df_label_cit.reset_index(drop=True, inplace=True)
+
 
         for i in range(int(len(df_label_cit['x']))):
             ax[1].text(df_label_cit['x'][i], df_label_cit['y'][i], str(df_label_cit['y'][i]), fontsize=7)
@@ -173,9 +216,118 @@ def PlotBilder(ref):
                        linewidth=0.5,
                        linestyle=':')
 
-    if df_comp.empty:
-        ax[0].plot(0, 0, label="нет совпадений по референсам")
+    elif df_comp_cit.empty:
+
+        ax[1].plot(0, 0, label="нет совпадений по референсам")
+
+        df_comp = checker_rule(df_comp)
+        df_comp.reset_index(drop=True, inplace=True)
+
+        annotate_mass_x = []
+        annotate_mass_y = []
+
+        df_ref_xcm = df_ref.loc[
+            (df_ref['ART_Model'].isin(df_comp['Model'])) | (df_ref['Name_Model'].isin(df_comp['Model']))]
+        df_ref_xcm.reset_index(drop=True, inplace=True)
+
+        for i in range(int(len(df_comp['Current_Price']))):
+            for j in range(int(len(df_ref_xcm))):
+                if df_comp['Model'][i] == df_ref_xcm['ART_Model'][j]:
+                    annotate_mass_x.append(df_comp['Time'][i])
+                    annotate_mass_y.append(df_comp['Current_Price'][i])
+
+        for i in range(int(len(df_ref_xcm['ART_Model']))):
+            label = df_comp['Model'][i] + " " + df_comp['Vendor'][i]
+
+            ax[0].plot(df_comp['Time'].loc[df_comp['Model'] == df_ref_xcm['ART_Model'][i]],
+                       df_comp['Current_Price'].loc[df_comp['Model'] == df_ref_xcm['ART_Model'][i]], '.-', label=label,
+                       linewidth=2)
+
+            data_label = {'x': annotate_mass_x, 'y': annotate_mass_y}
+            df_label = pd.DataFrame(data_label)
+            df_label.reset_index(drop=True, inplace=True)
+
+
+        for i in range(int(len(df_label['x']))):
+            ax[0].text(df_label['x'][i], df_label['y'][i], str(df_label['y'][i]), fontsize=7)
+            ax[0].set_title("Динамика цен Xcom-shop.ru", fontsize=15)
+            ax[0].set_ylabel("Цена (₽)", fontsize=15)
+            plt.xticks(rotation=35)
+
+            ax[0].grid(which='major', color='k',
+                       linewidth=0.5
+                       )
+
+            ax[0].minorticks_on()
+
+            ax[0].grid(which='minor', color='grey',
+                       linewidth=0.5,
+                       linestyle=':')
+
     else:
+
+        df_comp = checker_rule(df_comp)
+        df_comp_cit = checker_rule(df_comp_cit)
+        df_comp_cit.reset_index(drop=True, inplace=True)
+        df_comp.reset_index(drop=True, inplace=True)
+
+        df_ref_xcm = df_ref.loc[
+        (df_ref['ART_Model'].isin(df_comp['Model'])) | (df_ref['Name_Model'].isin(df_comp['Model']))]
+        df_ref_xcm.reset_index(drop=True, inplace=True)
+        df_ref_cit = df_ref.loc[
+        (df_ref['ART_Model'].isin(df_comp_cit['Model'])) | (
+            df_ref['Name_Model'].isin(df_comp_cit['Model']))]
+        df_ref_cit.reset_index(drop=True, inplace=True)
+
+
+    ###Проверка на заполненность датафрема проверки
+
+        annotate_mass_x_cit = []
+        annotate_mass_y_cit = []
+
+        ###Создается массив для аннотаций annotatr_mass_x(y)_cit для магазина citilink.ru
+        ###Представляет собой координаты для графика надо точками, чтобы указывать цены в точке
+        for i in range(int(len(df_comp_cit['Current_Price']))):
+            for j in range(int(len(df_ref_cit))):
+                if (df_comp_cit['Model'][i] == df_ref_cit['ART_Model'][j]) or (
+                        df_comp_cit['Model'][i] == df_ref_cit['Name_Model'][j]):
+                    annotate_mass_x_cit.append(df_comp_cit['Time'][i])
+
+                    annotate_mass_y_cit.append(df_comp_cit['Current_Price'][i])
+
+        for j in range(int(len(df_ref_cit['ART_Model']))):
+            label = df_comp_cit['Model'][j] + " " + df_comp_cit['Vendor'][j]
+            data_x = df_comp_cit['Time'].loc[(df_comp_cit['Model'] == df_ref_cit['Name_Model'][j]) | (
+                    df_comp_cit['Model'] == df_ref_cit['ART_Model'][j])]
+            data_y = df_comp_cit['Current_Price'].loc[(df_comp_cit['Model'] == df_ref_cit['Name_Model'][j]) | (
+                    df_comp_cit['Model'] == df_ref_cit['ART_Model'][j])]
+            ax[1].plot(data_x, data_y, '.-', label=label, linewidth=2)
+
+            data_label_cit = {'x': annotate_mass_x_cit, 'y': annotate_mass_y_cit}
+        df_label_cit = pd.DataFrame(data_label_cit)
+        df_label_cit.reset_index(drop=True, inplace=True)
+
+
+        for i in range(int(len(df_label_cit['x']))):
+            ax[1].text(df_label_cit['x'][i], df_label_cit['y'][i], str(df_label_cit['y'][i]), fontsize=7)
+
+            ax[1].set_title("Динамика цен Citilink.ru", fontsize=15)
+            ax[1].set_ylabel("Цена (₽)", fontsize=15)
+            plt.xticks(rotation=35)
+
+            ax[1].grid(which='major', color='k',
+                       linewidth=0.5
+                       )
+
+            ax[1].minorticks_on()
+
+            ax[1].grid(which='minor', color='grey',
+                       linewidth=0.5,
+                       linestyle=':')
+
+
+
+
 
         annotate_mass_x = []
         annotate_mass_y = []
@@ -196,6 +348,7 @@ def PlotBilder(ref):
             data_label = {'x': annotate_mass_x, 'y': annotate_mass_y}
             df_label = pd.DataFrame(data_label)
             df_label.reset_index(drop=True, inplace=True)
+
 
         for i in range(int(len(df_label['x']))):
             ax[0].text(df_label['x'][i], df_label['y'][i], str(df_label['y'][i]), fontsize=7)
